0b563a0cb9bf324607132179361d32d3a2f1dcfa
Naiki00 <nicrad@st.amu.edu.pl>
2021-03-13 17:44:55 +0100

    added files

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,16 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+    final static Logger log = Logger.getLogger(HelloWorld.class);
+
     public static void main(String [ ] args) {
+
         System.out.println("Hello World");
+
+        log.info("Welcome in Java World");
     }
+
 }
